b5788afa920bc4ebe5765e28d672c66bb6ec31b5
jonbaxt <jonbaxt@gmail.com>
2018-05-02 23:21:08 -0600

    Finished, but couldn't test

--- index.js
+++ index.js
@@ -1,8 +1,24 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const massive = require('massive');
+require('dotenv').config();
+const controller = require('./controller');
 
 const app = express();
+massive( process.env.CONNECTION_STRING ).then(dbInstance =>  {
+    app.set( 'db', dbInstance)
+
+// dbInstance.new_planes()
+// .then( planes => console.log( planes ) )
+// .catcH(err => console.log( err ));
+
+// dbInstance.get_planes()
+//     .then( planes => console.log( planes ))
+//     .catch( err => console.log( err ) );
+
+});
+
 app.use( bodyParser.json() );
 app.use( cors() );
 
